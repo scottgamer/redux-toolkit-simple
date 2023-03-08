c3d019d6aac4f38fbbac91aad6cabeff2b863785
rmunoz <richosojason@msn.com>
2023-03-08 12:01:41 -0500

    chore: add counter slice

---
+++ src/redux/counter.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// actions creators generated from each reducer function
+export const { decrement, increment, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
