24d6fc315707ff6a5ad274ebe9a6d90c958d5363
rmunoz <richosojason@msn.com>
2023-03-10 08:48:02 -0500

    chore: use redux hooks in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
 import "./App.css";
+import { decrement, increment } from "./redux/counter";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const { value } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="App">
-      <h1> The count is: {count}</h1>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
-      <button onClick={() => setCount(count + 1)}>increment</button>
+      <h1> The count is: {value}</h1>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
     </div>
   );
 };
