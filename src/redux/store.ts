e9b77a60d20a2d6972745ed5d48bc3256bbb0ae7
rmunoz <richosojason@msn.com>
2023-03-10 08:46:52 -0500

    chore: define RootState and AppDispatch types

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,6 +7,10 @@ const store = configureStore({
   },
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+
+// Inferred types for dispatching actions
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
