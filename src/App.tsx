8d5eb14d48ea4accc32d0f1ff11253a2d03a4907
rmunoz <richosojason@msn.com>
2023-04-06 15:56:46 -0500

    chore: add dispatch function to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { decrement, increment } from "./redux/counter";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 const App = () => {
@@ -11,6 +11,9 @@ const App = () => {
       <h1> The count is: {value}</h1>
       <button onClick={() => dispatch(decrement())}>decrement</button>
       <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        increment by 33
+      </button>
     </div>
   );
 };
